00795d34e1a7ae6f0f39912f80643aff64b31c01
Wutipong Wongsakuldej <wutipong@trueaxiongames.com>
2021-02-14 18:29:41 +0700

    move the gl constants up.

--- main.cpp
+++ main.cpp
@@ -1,52 +1,47 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <imgui.h>
 #include <cstdio>
+#include <glm/glm.hpp>
+#include <imgui.h>
 
-#include "imgui_impl/imgui_impl_opengl3.h"
 #include "imgui_impl/imgui_impl_glfw.h"
+#include "imgui_impl/imgui_impl_opengl3.h"
 
-constexpr int WIDTH = 800;
-constexpr int HEIGHT = 600;
+constexpr int WindowWidth = 800;
+constexpr int WindowHeight = 600;
 
-int main(int argc, char **argv)
-{
-  //init GLFW
-  if (!glfwInit())
-  {
+constexpr int GlMajorVersion = 3;
+constexpr int GlMinorVersion = 3;
+constexpr char GlslVersion[] = "#version 330";
+
+constexpr glm::vec4 ClearColor = {0.33f, 0.67f, 1.0f, 1.0f};
+
+int main(int argc, char **argv) {
+  if (!glfwInit()) {
     std::printf("GLFW initialisation failed!");
     glfwTerminate();
     return 1;
   }
 
-  const char *glsl_version = "#version 330";
-  //Setup GLFW window properties
-  //OpenGL version (using 3.3)
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-
-  //Core Profile = No Backwards Compatibility
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, GlMajorVersion);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, GlMinorVersion);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  //Allow forward compatibility
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-  GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", NULL, NULL);
+  GLFWwindow *window = glfwCreateWindow(WindowWidth, WindowHeight,
+                                        "glfw-project-template", NULL, NULL);
 
-  if (!window)
-  {
+  if (!window) {
     std::printf("GLFW window creation failed!");
     glfwTerminate();
     return 1;
   }
 
-  //Set context for GLEW to use
   glfwMakeContextCurrent(window);
 
-  //Allow modern extension features
   glewExperimental = GL_TRUE;
 
-  if (glewInit() != GLEW_OK)
-  {
+  if (glewInit() != GLEW_OK) {
     printf("GLEW initialisation failed!");
     glfwDestroyWindow(window);
     glfwTerminate();
@@ -61,11 +56,9 @@ int main(int argc, char **argv)
   ImGui::StyleColorsDark();
 
   ImGui_ImplGlfw_InitForOpenGL(window, true);
-  ImGui_ImplOpenGL3_Init(glsl_version);
+  ImGui_ImplOpenGL3_Init(GlslVersion);
 
-  while (!glfwWindowShouldClose(window))
-  {
-    //Get + Handle user input events
+  while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
 
     ImGui_ImplOpenGL3_NewFrame();
@@ -73,31 +66,32 @@ int main(int argc, char **argv)
     ImGui::NewFrame();
 
     // Begin UI
-    /* ImGui::Begin("Hello World");
-     * ImGui::Button("Push Me If You Can");
-     * ImGui::End();
-     */ 
+    ImGui::Begin("Greetings");
+    ImGui::Button("Hello World");
+    ImGui::End();
     // End UI
 
     ImGui::EndFrame();
     ImGui::Render();
+
     int display_w, display_h;
     glfwGetFramebufferSize(window, &display_w, &display_h);
     glViewport(0, 0, display_w, display_h);
 
-    //Clear window
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClearColor(ClearColor.r, ClearColor.g, ClearColor.b, ClearColor.a);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    //draw here
-    /* 
-    * 
-    *
-    *
-    * 
-    */
-    //end draw
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    // draw here
+    /*
+     *
+     *
+     *
+     *
+     */
+    // end draw
+
     glfwSwapBuffers(window);
   }
 
